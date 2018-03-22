6f0ce50e5eab98b5383ba0f6106ac8875cfb3dd2
Adrixop95 <adrixop95@me.com>
2018-03-22 16:19:32 +0100

    script

--- docker.sh
+++ docker.sh
@@ -11,11 +11,17 @@ sudo dpkg --configure -a
 sudo apt-get install -f -a
 
 #Set up repo
-sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 #Install Docker CE
 sudo apt-get update
-sudo apt-get install docker-ce
+sudo apt-get install docker-ce -y
+
+#Install Ubuntu GNU/Linux on Docker Containder
+sudo docker pull ubuntu
+
+#Start this containder
+sudo docker run ubuntu
