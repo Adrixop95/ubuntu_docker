278b2f573ac66ab425365a1cbd2562e6c42b5854
Adrixop95 <adrixop95@me.com>
2018-03-22 17:22:44 +0100

    script

--- docker.sh
+++ docker.sh
@@ -23,8 +23,8 @@ sudo apt-get install docker-ce -y
 #Install Ubuntu GNU/Linux on Docker Containder
 sudo docker pull ubuntu
 
-#Start this containder
-sudo docker run -itd ubuntu
+#Start this containder with port 80 for apache server
+sudo docker run -itd -p 80:80 ubuntu
 
 #Get container name
 temp_name="$(docker ps --format "{{.Names}}")"
@@ -34,3 +34,8 @@ sudo echo "${docker_name}"
 #Rename container
 sudo docker rename ${docker_name} ubuntu_apache
 
+#Execute commands on container (install apache2)
+sudo docker exec ubuntu_apache apt-get update
+sudo docker exec ubuntu_apache apt-get upgrade -y
+sudo docker exec ubuntu_apache apt-get install apache2 -y
+sudo docker exec ubuntu_apache /etc/init.d/apache2 restart
